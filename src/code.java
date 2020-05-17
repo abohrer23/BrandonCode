ccba3a00c69b04b0a567ee94406c8cfc093db5c5
Brandon <Brandon@Koy-PC>
2020-05-17 18:35:51 -0500

    life is unending agony it hurts lane it hurts plesasdee end me i 'amsdf i

--- src/code.java
+++ src/code.java
@@ -4,23 +4,50 @@ import java.util.Scanner;
 public class code {
 	public static void main(String[] args){
 		
-        Scanner in = new Scanner(System.in);
-    	System.out.println("Enter a word here:");
-		String s = in.nextLine();
-		System.out.println("Enter a number here:");
-		int one = in.nextInt();
-		System.out.println("Enter a second number here:");
-		int two = in.nextInt();
-				
-		System.out.println("You entered: " + s);
-		System.out.println("You entered: " + one + ", " + two);
-		in.close();
+//        Scanner in = new Scanner(System.in);
+//    	System.out.println("Enter a word here:");
+//		String s = in.nextLine();
+//		System.out.println("Enter a number here:");
+//		int one = in.nextInt();
+//		System.out.println("Enter a second number here:");
+//		int two = in.nextInt();
+//				
+//		System.out.println("You entered: " + s);
+//		System.out.println("You entered: " + one + ", " + two);
+//		in.close();
+//		
+////		System.out.println();
+//
+//		int sum = add(one,two);
+//		
+//		System.out.println("The sum of "+ one +" and "+ two +" is "+sum+ ", bitch" );
+//		for (int i=0 ; i<20000 ; i++) {
+//			System.out.println( "whore") ;
+//		}
 		
-//		System.out.println();
-
-		int sum = add(one,two);
+//		
+//		int j=0 ; 
+//		while (j<2) {
+//			System.out.println ( "love you lane :)");
+//			j++;
+//		}
+//		
+//		double g=Math.random() ;
+//		System.out.println ("your number is"+g);
+		for (int f=0 ; f<1 ; f++)
+			{ double i=Math.random()*20; 
+			int e=(int) Math.ceil(i);
+			System.out.println ("the number is "+e);
+			if(e<10) {
+				System.out.println ("garbage");
+			} 
+			else {
+			System.out.println ("nice");
+				}
+				}
 		
-		System.out.println("The sum of "+ one +" and "+ two +" is "+sum);
+		
+		
 		
 	}
 	
