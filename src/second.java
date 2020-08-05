f7df5e5a499194eef580b683deb9477e2152596c
abohrer23 <54337892+abohrer23@users.noreply.github.com>
2020-08-05 01:14:48 -0500

    hehe coomit :)

---
+++ src/second.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class second {
+
+	public static void main(String[] args) {
+		rolling();
+	}
+
+	private static void rolling() {
+		Scanner in = new Scanner(System.in);
+		boolean roll = true;
+		
+		while(roll) {
+			//first goal: can we make better dice?
+			//next two lines are from last time
+	    	System.out.println("Enter your command here:");
+			String s = in.nextLine(); //'r2d6'
+			
+			
+			//we have a string, now what? arrays, babey!
+			
+			
+			//now let's make this info useful
+
+
+			//okay, so how do we roll?
+		
+			
+		}
+	}
+}
+	
